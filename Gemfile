b802a08062e9afc91f2157e59dce1706b06ee9d1
Cody Hill <codyhill@bdogm.com>
2011-03-04 17:30:24 -0600

    Added about page

--- Gemfile
+++ Gemfile
@@ -32,4 +32,5 @@ gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
  group :test do
 	gem 'rspec', '2.5.0'
     gem 'webrat', '0.7.1'
+	gem 'spork', '0.9.0.rc4'
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+  
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -15,5 +17,11 @@ describe PagesController do
       response.should be_success
     end
   end
-
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
 end
