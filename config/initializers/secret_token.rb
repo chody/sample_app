f9702e0fbe3ba78cf00efe2121121926229edac7
Cody Hill <codyhill@bdogm.com>
2011-03-04 09:41:24 -0600

    this is the sample app where I build 'NoTwitter'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'e93450cb3af0339ce210ba33eae135269301a997113015fd53125a7b13b5c558319a1a89e38d76552a68cce0ff41a326f741aa26c21dfbd1fbc034b5ab193b7a'
