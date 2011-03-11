e05fc970399921b518725352d254bfe94f9a70aa
Cody Hill <codyhill@bdogm.com>
2011-03-11 10:04:47 -0600

    Most updated verstion

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
   
+  def logo
+    #puts logo on main page
+   @logo = image_tag("logo.png", :alt => "Sample App", :class => "round") 
+  end
+  
   def title
     #Returns title on a per page basis.
     base_title = "Ruby on Rails Tutorial Sample App"
