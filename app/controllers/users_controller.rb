83b72e06c7df8156acc32e57d5be58a31cc6fd0c
Cody Hill <codyhill@bdogm.com>
2011-03-16 12:19:15 -0500

    User Signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,26 @@
 class UsersController < ApplicationController
   def new
     @title = "Sign up"
-
-  end
+    @user = User.new
+  end # end of def new
   
   def show
     @user = User.find(params[:id])
     @title = @user.name
-  end
-
-end
+  end #end of def show
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      
+      #handle a successful save
+      flash[:success] = "Welcome to NOTwitter!"
+      redirect_to @user
+    else
+      #render errors and title screen
+      @title = "Sign up"
+      render 'new'
+    end #end of if
+  end # End of Def Create
+  
+end  #End of Controller
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe "Users" do
+  #describe "GET /users" do
+   # it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+    #  get users_index_path
+     # response.status.should be(200)
+    #end
+    describe "signup" do
+    
+    describe "success" do
+      it "should make a new user" do
+        lambda do
+          visit signup_path
+          fill_in "Name", :with => "Example User"
+          fill_in "Email", :with => "foobar@example.com"
+          fill_in "Password", :with => "example"
+          fill_in "Confirm Password", :with => "example"
+          click_button
+          response.should have_selector("div.flash.success", :content => "Welcome")
+          response.should render_template('users/show')
+        end.should change(User, :count).by(1)
+      end
+      
+    describe "failure" do
+      
+        it "should not make a new user" do
+          lambda do
+          visit signup_path
+          fill_in "Name", :with => ""
+          fill_in "Email", :with => ""
+          fill_in "Password", :with => ""
+          fill_in "Confirm Password", :with => ""
+          click_button
+          response.should render_template('users/new')
+          response.should have_selector('div#error_explanation')
+          end.should_not change(User, :count)
+        end
+      end
+    end
+  end
+end
+
